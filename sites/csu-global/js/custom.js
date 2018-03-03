d954a1b75fe714662355a60785e67c65d03511e9
guitarbyrob <guitarbyrob@gmail.com>
2018-03-03 03:24:30 -0500

    csu global

---
+++ sites/csu-global/js/custom.js
@@ -0,0 +1,6 @@
+// Add here all your JS customizations
+
+$(document).ready (function() {
+	$("input[type=submit]").attr('disabled','disabled');
+	$("a[href='https://application.csuglobal.edu']").bind('click', false);
+});
