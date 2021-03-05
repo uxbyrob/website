b455c493a1667a441bf15321fc3bb3e3807f89bb
uxbyrob <guitarbyrob@gmail.com>
2021-03-04 23:32:26 -0700

    Update designr.js

--- js/designr.js
+++ js/designr.js
@@ -205,7 +205,7 @@ $(document).ready(function() {
 	==============================================*/
 	$(window).resize(function(){
 		//$('.jumbotron').height($(window).height());
-		$('.message-box').css({'marginTop':$(window).height()*0.1});
+		$('.message-box').css({'marginTop':$(window).height()*0.15});
 		scrollSpyRefresh();
 		waypointsRefresh();
 	});
